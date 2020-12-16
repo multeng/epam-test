838e0229b23d60d11a51c0cc4896a64a8b1b001c
Vladimir Kormakov <cenizo.kht@gmail.com>
2020-12-16 23:52:19 +0300

    fix: fade-in animation; add: form validation

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-const root = document.querySelector('.root')
-document.addEventListener("DOMContentLoaded", () => {
-  root.classList.add('visible');
-})
+const root = document.querySelector('.root');
+const formContainer = document.querySelector('.form-container');
+const form = document.querySelector('.form');
+const login = document.getElementById('login');
+const password = document.getElementById('password');
+
+window.onload = function () {
+  fadeInRoot();
+};
+
+function fadeInRoot() {
+  root.addEventListener('transitionend', fadeInLoginForm());
+  root.classList.add('fade-in');
+}
+
+function fadeInLoginForm() {
+  formContainer.classList.add('fade-in');
+}
+
+function checkEmailValidation() {
+  login.checkValidity() ? null : login.reportValidity();
+}
+function checkPasswordValidation() {
+  password.checkValidity() ? null : password.reportValidity();
+}
+function checkSubmitFormValidation(e) {
+  e.preventDefault();
+  form.checkValidity() ? form.submit() : form.reportValidity();
+}
+
+login.addEventListener('blur', checkEmailValidation);
+password.addEventListener('blur', checkPasswordValidation);
+form.addEventListener('submit', checkSubmitFormValidation);
